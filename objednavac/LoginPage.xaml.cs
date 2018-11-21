119b71b11041b3c468ae00a0c24d673e4e2b1dc5
Vit Hanibal <vit.hanibal41@gmail.com>
2018-11-21 08:39:25 +0100

    prnvi

---
+++ objednavac/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace objednavac
+{
+	/// <summary>
+	/// Interaction logic for LoginPage.xaml
+	/// </summary>
+	public partial class LoginPage:Page
+	{
+		MainWindow mainWindow;
+
+		public LoginPage(MainWindow mainWindow)
+		{
+			InitializeComponent();
+
+			this.mainWindow = mainWindow;
+		}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+			var nav = NavigationService.GetNavigationService(this);
+			nav.Navigate(mainWindow);
+		}
+	}
+}
